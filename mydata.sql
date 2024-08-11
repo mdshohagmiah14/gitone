8dff39ae23c3f4bcde902c48bce740b71522a9d3
mdshohagmiah14 <111840711+mdshohagmiah14@users.noreply.github.com>
2024-08-11 11:01:11 -0400

    Add files via upload

---
+++ mydata.sql
@@ -0,0 +1,16 @@
+USE mydatabase;
+CREATE TABLE STUDENT(SNO INT(5),SNAME VARCHAR(15), MARKS INT(3));
+DESCRIBE STUDENT;
+INSERT INTO STUDENT VALUES(100, 'SHOHAG', 90);
+DESCRIBE STUDENT;
+SELECT * FROM  STUDENT;
+use mydatabase;
+SELECT * FROM  STUDENT;
+use firstone;
+
+
+
+create database firstone;
+use firstone;
+create table odhora (Id int (10), name varchar (20), marks int(3));
+
